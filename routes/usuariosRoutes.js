4bd65194430dfb67ca9ba6fe134a443f957955fc
ZoilaSanchez <lopez@LAPTOP-V96DJ8NN.localdomain>
2022-03-31 21:31:27 -0600

    Correcion de metodos

--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -2,7 +2,7 @@ const {Router} = require('express')
 
 const router = Router();
 
-const {getUsuario,crearUsuario,confirmar_user,actualizar,actualizar_email,actualizar_password,nuevaPass,comprobar_token,login,recuperarCodigo} = require('../controllers/usuarioControllers')
+const {getUsuario,crearUsuario,confirmar_user,actualizar_info,actualizar_email,actualizar_password,nuevaPass,comprobar_token,login,recuperarCodigo} = require('../controllers/usuarioControllers')
 
 router.route('/')
 .get(getUsuario)
@@ -11,13 +11,13 @@ router.route('/')
 router.route('/confirmar/:token').
 get(confirmar_user)
 
-router.route('/actualizar/:id').
-put(actualizar)
+router.route('/actualizar-info/:codigo').
+put(actualizar_info)
 
-router.route('/email').
+router.route('/email/:codigo').
 put(actualizar_email)
 
-router.route('/actualizar_pass').
+router.route('/act-pass').
 put(actualizar_password)
 
 router.route('/act-pass/:token').get(comprobar_token).put(nuevaPass)
